3fa61d28223e8827e11abf6e3aaae6d6fe88c7e4
juaeslop <juaeslop@bancolombia.com.co>
2021-06-13 17:49:39 -0500

    adelanto del proceso de busquedad de hospedaje

--- PruebaDevco/src/main/java/com/travelocity/tasks/FillTheInformation.java
+++ PruebaDevco/src/main/java/com/travelocity/tasks/FillTheInformation.java
@@ -7,8 +7,7 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 
-import static com.travelocity.userinterfaces.HomePage.BTN_LOCATION;
-import static com.travelocity.userinterfaces.HomePage.INP_DESTINATION;
+import static com.travelocity.userinterfaces.HomePage.*;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
@@ -24,10 +23,33 @@ public class FillTheInformation implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
+
         actor.attemptsTo(WaitUntil.the(BTN_LOCATION, isVisible()).forNoMoreThan(3).seconds(),
                 Click.on(BTN_LOCATION),
-                Enter.theValue(destination).into(INP_DESTINATION));
+                Enter.theValue(destination).into(INP_DESTINATION),
+                Click.on(BTN_SELECT_LOCATION),
+                Click.on(BTN_TRAVELERS)
+        );
+        String numberAdults = NUMBER_ADULTS.resolveFor(actor).getValue();
 
+        if(!(Integer.parseInt(numberAdults) == adults)){
+            if(Integer.parseInt(numberAdults) > adults){
+                actor.attemptsTo(Click.on(BTN_DECREASE_ADULTS));
+            }else{
+                for (int i=1; i <= (adults-Integer.parseInt(numberAdults)); i++)
+                {
+                    actor.attemptsTo(Click.on(BTN_INCREASE_ADULTS));
+                }
+            }
+        }
+        if (children>0){
+            for (int i=1; i <= children; i++)
+            {
+                actor.attemptsTo(Click.on(BTN_INCREASE_CHILDREN));
+            }
+        }
+        actor.attemptsTo(Click.on(BTN_CONFIRM_TRAVELERS),
+                Click.on(BTN_SEARCH));
     }
 
     public static FillTheInformation ofStay(String destination, String fchIni, String fhcFin, int adults, int children ){
--- PruebaDevco/src/main/java/com/travelocity/userinterfaces/HomePage.java
+++ PruebaDevco/src/main/java/com/travelocity/userinterfaces/HomePage.java
@@ -16,10 +16,17 @@ public class HomePage {
     public static final Target BTN_LANGUAGE = Target.the("button to change the language of the page").locatedBy("//button[@class='uitk-button uitk-button-medium uitk-button-has-text']");
     public static final Target BTN_LOCATION = Target.the("button to search the location").locatedBy("//button[@class='uitk-faux-input']");
     public static final Target BTN_SELECT_LOCATION = Target.the("button to select the location").locatedBy("//*[@id=\"location-field-destination-menu\"]/div[2]/ul/li[1]/button");
+    public static final Target BTN_TRAVELERS = Target.the("button to select the travelers").locatedBy("//button[@class='uitk-fake-input uitk-form-field-trigger']");
+    public static final Target BTN_DECREASE_ADULTS = Target.the("button to decrease the number of adults").locatedBy("//*[@id=\"adaptive-menu\"]/div[2]/div/section/div[1]/div[2]/div/button[1]");
+    public static final Target BTN_INCREASE_ADULTS = Target.the("button to increase the number of adults").locatedBy("//*[@id=\"adaptive-menu\"]/div[2]/div/section/div[1]/div[2]/div/button[2]");
+    public static final Target BTN_INCREASE_CHILDREN = Target.the("button to increase the number of children").locatedBy("//*[@id=\"adaptive-menu\"]/div[2]/div/section/div[1]/div[3]/div/button[2]");
+    public static final Target BTN_CONFIRM_TRAVELERS = Target.the("button to confirm the number of the travelers").locatedBy("//button[@class='uitk-button uitk-button-large uitk-button-fullWidth uitk-button-has-text uitk-button-primary uitk-button-floating-full-width']");
+    public static final Target BTN_SEARCH = Target.the("button to search the results").locatedBy("//*[@id=\"wizard-hotel-pwa-v2-1\"]/div[2]/div[2]/button");
 
     //TITLE
     public static final Target TITLE_TRAVELS1 = Target.the("The title that indicates that you are in a search different of travel").locatedBy("//div[@class='uitk-type-display-700']");
     public static final Target TITLE_TRAVELS2 = Target.the("The title that indicates that you are in a search different of travel").locatedBy("//h1[@class='uitk-heading-3']");
+    public static final Target NUMBER_ADULTS = Target.the("number of the adults").located(By.id("adult-input-0"));
 
     //INPUT
     public static final Target INP_DESTINATION = Target.the("The input of the destination").located(By.id("location-field-destination"));
